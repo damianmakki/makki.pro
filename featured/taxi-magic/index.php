6b08bab11e79ef76a350c37f9ab1214111ee498a
Damian Makki <damian@makki.pro>
2014-12-31 15:45:18 -0500

    First commit.

---
+++ featured/taxi-magic/index.php
@@ -0,0 +1,63 @@
+<?$root = $_SERVER['DOCUMENT_ROOT'] . '/demo/makki';?>
+
+<? $pageTitle='Taxi Magic'; ?>
+<? $pageDescription='Taxi Magic - various web and print projects with Damian Makki of makki.pro.'; ?>
+
+<?php include($root . '/components/header.php'); ?>
+
+    <div class="container content-container">
+    
+    	<!-- work -->
+		<div class="row section">
+			<div class="span2 project"><h3>Taxi Magic</h3><p>The #1 taxi-booking app in the country. Taxi Magic is one of my favorite clients. We've completed a ton of projects together and thanks to their incredible team, they're a pleasure to work with. Though this is a small sampling, we've completed a number of print projects ranging from holiday cards to custom posters, some great email campaigns, and even a launch page for their New York City launch in June, 2013.</p></div>
+			<div class="span9 offset1">
+			
+				<!-- Image -->
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Taxi Magic NYC Posters (Series 1 of 3)">
+						        <span data-src="img/NYCPoster.jpg"></span>
+						</span> 
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Taxi Magic Bazaar Noir coupon cards">
+						        <span data-src="img/PalmCardMockup_v1.jpg"></span>
+						</span> 
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Taxi Magic New York City 'LES' advertisement">
+						        <span data-src="img/LES.jpg"></span>
+						</span> 
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Taxi Magic New York City signup microsite">
+						        <span data-src="img/NYCSignup.jpg"></span>
+						</span> 
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Taxi Magic New York City brand ambassador custom shirts">
+						        <span data-src="img/NYCShirt.jpg"></span>
+						</span> 
+					</div>
+				</div>
+				
+		</div>
+		<!-- /work -->
+	</div>
+	</div> <!-- /container -->
+	
+	<?php include($root . '/featured/featured-components.php'); ?>
+	
+	<?php include($root . '/components/footer.php'); ?>
---
+++ featured/tequila-avion/index.php
@@ -0,0 +1,78 @@
+<?$root = $_SERVER['DOCUMENT_ROOT'] . '/demo/makki';?>
+
+<? $pageTitle='Tequila Avión'; ?>
+<? $pageDescription='Tequila Avión - a website redesign project with Damian Makki of makki.pro.'; ?>
+
+<?php include($root . '/components/header.php'); ?>
+
+    <div class="container content-container">
+    
+    	<!-- work -->
+		<div class="row section">
+			<div class="span2 project"><h3>Tequila Avión</h3><p>If you've ever watched <i>Entourage</i>, you know about Tequila Avión. The ultra-premium tequila brand approached me in early 2013 to collaborate on a website redesign.</p><p><a target="_blank" href="#">New site launching soon!</a></p></div>
+			<div class="span9 offset1">
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Tequila Avion and Damian Makki">
+						        <span data-src="img/intro.jpg"></span>
+						        <span data-src="img/intro.jpg" data-media="(min-device-pixel-ratio: 2.0)"></span>
+						        <span data-src="img/intro.jpg" data-media="(max-width: 767px)"></span>
+						</span> 
+					</div>
+				</div>
+				<div class="row">
+					<div class="comments span9"><p>The old Tequila Avión site was outdated - faux wood backgrounds, carousels galore, a dismal drink showcase section, and a downright pitiful news roll.</p></div>
+				</div>
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Old Tequila Avion home page">
+							<span data-src="img/old1.jpg"></span>
+						</span> 
+					</div>	
+				</div>
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Old Tequila Avion drinks page">
+							<span data-src="img/old3.jpg"></span>
+						</span> 
+					</div>	
+				</div>
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="Old Tequila Avion news page">
+							<span data-src="img/old2.jpg"></span>
+						</span> 
+					</div>	
+				</div>
+				<div class="row">
+					<div class="comments span9"><p>In collaboration with some key members of the Avión team, we decided on an extremely minimal and clean aesthetic. We implemented some nifty animations, some really great typography, a brand new drink filter, and a dynamic masonry-powered news roll.</p></div>
+				</div>
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="New Tequila Avion drinks page">
+							<span data-src="img/new3.jpg"></span>
+						</span> 
+					</div>	
+				</div>
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="New Tequila Avion navigation bar">
+							<span data-src="img/new-navbar.jpg"></span>
+						</span> 
+					</div>	
+				</div>
+				<div class="row">
+					<div class="block span9">
+						<span data-picture data-alt="New Tequila Avion news page">
+							<span data-src="img/new2.jpg"></span>
+						</span> 
+					</div>	
+				</div>
+			</div>
+		</div>
+		<!-- /work -->
+	</div> <!-- /container -->
+	
+	<?php include($root . '/featured/featured-components.php'); ?>
+	
+	<?php include($root . '/components/footer.php'); ?>
