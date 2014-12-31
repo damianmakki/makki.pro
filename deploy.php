0a4d311e00ad52b140a4934cba98aed4c5b7f06f
Damian Makki <damian@makki.pro>
2014-12-31 16:00:12 -0500

    Added deploy script

---
+++ deploy.php
@@ -0,0 +1,17 @@
+<?php
+$commands = [
+  'echo $PWD',
+  'whoami',
+  'git branch -r',
+  'git pull -v',
+  'git status',
+  'git submodule sync',
+  'git submodule update',
+  'git submodule status'
+];
+
+foreach ( $commands as $command )
+{
+  $tmp = shell_exec( $command );
+  echo '<pre>' . htmlentities( trim( $tmp ) ) . "\n</pre>";
+}
