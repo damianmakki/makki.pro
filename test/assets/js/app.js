fc8573d94798f464c9b5b0055d46ba08e95bf8e2
Damian Makki <damian@makki.pro>
2015-08-16 16:10:58 -0400

    Updated to slider JS.

--- test/assets/js/app.js
+++ test/assets/js/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 	$('#carousel').slick({
 		dots: true,
+		lazyLoad: 'ondemand',
 		infinite: true,
   		slidesToShow: 1,
   		slidesToScroll: 1,
-		speed: 500,
-		respondTo: 'min',
-		centerMode: true
+		speed: 500
 	});
 });
