5a8b8bd4c29e4b2ca93f03040d8fcff2e49cff25
Damian Makki <damian@makki.pro>
2015-08-16 15:10:03 -0400

    Case study updates.

--- test/assets/js/app.js
+++ test/assets/js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 	$('#carousel').slick({
 		dots: true,
-		//lazyLoad: 'ondemand',
 		infinite: true,
   		slidesToShow: 1,
   		slidesToScroll: 1,
-		speed: 500
+		speed: 500.
+		respondTo: 'min'
 	});
 });
