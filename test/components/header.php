0b2248ad7f39d85298cd698a399835396fbfabe4
Damian Makki <damian@makki.pro>
2015-08-18 12:31:12 -0400

    More dynamic pieces.

---
+++ test/components/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title><?php echo $pageTitle ?> | Designer and Developer</title>
+	<meta name="description" content="<?php echo $pageDescription ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+	<div class="nav">
+
+		<div class="menu-burger" title="Menu"><img src="/test/assets/images/menu.svg" ></div>
+		<span class="dm" title="Home">Damian Makki</span>
+		<span><?php echo $pageTitle ?></span>
+
+	</div>
---
+++ test/index.php
@@ -0,0 +1,13 @@
+<?php $pageTitle='Designer and Developer'; ?>
+<?php $pageDescription='Damian Makki is a creative web and print designer based out of New York City.'; ?>
+
+<?php include('components/header.php'); ?>
+
+<div class="sidebar"></div>
+<div class="content">
+
+	<h1>I'm Damian - independent web and print designer based out of New York City.</h1>
+
+</div>
+
+<?php include('components/footer.php'); ?>
