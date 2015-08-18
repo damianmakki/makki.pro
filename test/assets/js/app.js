214df4c4383544db3e793d04305464d17d8c59c4
Damian Makki <damian@makki.pro>
2015-08-18 14:08:15 -0400

    JS.

--- test/assets/js/app.js
+++ test/assets/js/app.js
@@ -7,14 +7,16 @@ $(document).ready(function(){
 		arrows: 		false
 	});
 
-	$(".nav").click(function(e) {
-
-		if( $('.menu').hasClass("fadeInUp") ) {
-			$('.menu').removeClass("fadeInUp").addClass("fadeOutDown");
-		} else {
-            ('.menu').addClass("fadeInUp");
-        }
-
-    });
+	$(".menu").click(function(e) {
+      if($(this).hasClass("open")) {
+         // if it's open then just close it
+         $(this).removeClass("open");
+      } else {
+         // if it's closed, then close everything else and open it
+         $(".more").removeClass("open");
+         $(this).addClass("open");
+      }
+      /* TODO: now do something similar with icon-arrow */
+   });
 
 });
