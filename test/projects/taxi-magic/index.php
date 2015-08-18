d1580c211f0f9469b8495ec6cac9fc808256a324
Damian Makki <damian@makki.pro>
2015-08-18 12:48:37 -0400

    Taxi Magic as a test project.

---
+++ test/projects/taxi-magic/index.php
@@ -0,0 +1,37 @@
+<?php $pageTitle='Case Study'; ?>
+<?php $pageDescription='Damian Makki is a creative web and print designer based out of New York City.'; ?>
+
+<?php include('../../components/header.php'); ?>
+
+<div class="content case-study">
+
+	<div class="meta">
+
+		<div class="section">
+			<label>Year</label>
+			<p>2013</p>
+		</div>
+
+		<div class="section">
+			<label>Roles</label>
+			<p>Designer, developer, print design.</p>
+		</div>
+
+		<div class="section">
+			<label>Overview</label>
+			<p>The #1 taxi-booking app in the country. Taxi Magic is one of my favorite clients. We've completed a ton of projects together and thanks to their incredible team, they're a pleasure to work with. Though this is a small sampling, we've completed a number of print projects ranging from holiday cards to custom posters, some great email campaigns, and even a launch page for their New York City launch in June, 2013.</p>
+		</div>
+
+	</div>
+
+	<div id="carousel">
+		<img src="img/LES.jpg">
+		<img src="img/NYCPoster.jpg">
+		<img src="img/NYCShirt.jpg">
+		<img src="img/NYCSignup.jpg">
+		<img src="img/PalmCardMockup_v1.jpg">
+	</div>
+
+</div>
+
+<?php include('../../components/footer.php'); ?>
