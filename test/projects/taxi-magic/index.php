2c687dc9c11de0d2cedfd400da2a3437b6e211fb
Damian Makki <damian@makki.pro>
2015-08-18 13:15:18 -0400

    Next and previous projects.

--- test/projects/taxi-magic/index.php
+++ test/projects/taxi-magic/index.php
@@ -22,14 +22,19 @@
 			<p>The #1 taxi-booking app in the country. Taxi Magic is one of my favorite clients. We've completed a ton of projects together and thanks to their incredible team, they're a pleasure to work with. Though this is a small sampling, we've completed a number of print projects ranging from holiday cards to custom posters, some great email campaigns, and even a launch page for their New York City launch in June, 2013.</p>
 		</div>
 
+		<div class="section pull-right">
+			<label>Next Project</label>
+			<p><a href="#">Taxi Magic</a></p>
+			<label>Previous Project</label>
+			<p><a href="#">Tequila Avion</a></p>
+		</div>
 	</div>
 
-
+	<img src="images/PalmCardMockup_v1.jpg">
 	<img src="images/LES.jpg">
 	<img src="images/NYCPoster.jpg">
 	<img src="images/NYCShirt.jpg">
 	<img src="images/NYCSignup.jpg">
-	<img src="images/PalmCardMockup_v1.jpg">
 
 </div>
 
