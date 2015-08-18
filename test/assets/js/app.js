a4c02aa52a1d3ae1f6b84f2f78c6710ac810c0f0
Damian Makki <damian@makki.pro>
2015-08-18 14:17:09 -0400

    Off click menu hide it.

--- test/assets/js/app.js
+++ test/assets/js/app.js
@@ -8,15 +8,24 @@ $(document).ready(function(){
 	});
 
 	$(".menu-burger").click(function(e) {
-      if($(".menu").hasClass("opened")) {
+		if($(".menu").hasClass("opened")) {
          // if it's open then just close it
          $(".menu").removeClass("opened");
          $(".menu").addClass("closed");
-      } else {
-         // if it's closed, then close everything else and open it
-         $(".menu").removeClass("closed");
-         $(".menu").addClass("opened");
-      }
-   });
+	     } else {
+	         // if it's closed, then close everything else and open it
+	         $(".menu").removeClass("closed");
+	         $(".menu").addClass("opened");
+	     }
+	 });
+
+
+	$("body").click(function() {
+		$(".menu").addClass("closed");
+	});
+
+	$(".menu").click(function(e) {
+		e.stopPropagation();
+	});
 
 });
