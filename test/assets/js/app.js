05690d1a3b785e5d9f3a0a2ccb00328f550ce0d9
Damian Makki <damian@makki.pro>
2015-08-18 14:06:34 -0400

    JS trials and style updates.

--- test/assets/js/app.js
+++ test/assets/js/app.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
+
 	$('#carousel').slick({
 		infinite: 		true,
-  		slidesToShow: 	1,
+		slidesToShow: 	1,
 		centerMode: 	true,
 		arrows: 		false
 	});
+
+	$(".nav").click(function(e) {
+
+		if( $('.menu').hasClass("fadeInUp") ) {
+			$('.menu').removeClass("fadeInUp").addClass("fadeOutDown");
+		} else {
+            ('.menu').addClass("fadeInUp");
+        }
+
+    });
+
 });
