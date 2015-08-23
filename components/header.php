86e8cfd264219b2a17f3d908b0c1d4d725257235
Damian Makki <damian@makki.pro>
2015-08-23 15:21:00 -0400

    Correct linking of styles.

--- components/header.php
+++ components/header.php
@@ -7,13 +7,13 @@
 	<meta name="description" content="<?php echo $pageDescription ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="stylesheet" href="../assets/css/style.css">
+	<link rel="stylesheet" href="http://www.makki.pro/assets/css/style.css">
 </head>
 <body>
 
 	<div class="nav">
 
-		<div class="menu-burger" title="Menu"><img src="../assets/images/menu.svg" ></div>
+		<div class="menu-burger" title="Menu"><img src="http://www.makki.pro/images/menu.svg" ></div>
 		<span class="dm" title="Home">Damian Makki</span>
 		<span><?php echo $pageTitle ?></span>
 		<div class="menu">
