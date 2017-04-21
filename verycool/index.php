b6e9c05e20622c5a8799a835af6ade635aa853c9
Damian Makki <damian@makki.pro>
2017-04-21 08:28:48 -0700

    Disabled Issue One.

--- verycool/index.php
+++ verycool/index.php
@@ -32,7 +32,8 @@
 
 			<div class="campaign--header"><span>April 21, 2017</span></div>
 			<div class="campaign--body"><img src="assets/images/issue-one-hero.jpg" /></div>
-			<a href="issue-one" class="button">Check Out Issue One</a>
+			<!--<a href="issue-one" class="button">Check Out Issue One</a>-->
+			<a href="issue-one" class="button disabled">Coming Soon</a>
 
 			</div>
 		</div>
