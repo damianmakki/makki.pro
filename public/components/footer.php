fb36f6b9756e87f29a07ba10af9659736bdb320f
Damian Makki <damian@makki.pro>
2019-01-30 11:39:45 -0800

    Update header and footer with new page description, schema, and new favicon.

--- public/components/footer.php
+++ public/components/footer.php
@@ -15,5 +15,32 @@
 		
 	</script>
 
+	<!-- Schema -->
+	<script type="application/ld+json">
+	{
+	  "@context": "http://schema.org/",
+	  "@type": "Person",
+	  "name": "Damian Makki",
+	  "url": "https://makki.pro",
+	  "image": "https://makki.pro/assets/images/damian-makki-profile.jpg",
+	  "sameAs": [
+	  	"https://instagram.com/damianmakki/",
+	  	"https://dribbble.com/damianmakki",
+	  	"https://github.com/damianmakki",
+	  	"https://www.linkedin.com/in/damianmakki",
+	  	"https://www.strava.com/athletes/6190541",
+	    "https://www.facebook.com/damianmakki",
+	    "https://twitter.com/damianmakki",
+	    "https://medium.com/@damianmakki",
+	    "https://ruckuslabs.co"
+	  ],
+	  "jobTitle": "Designer and Developer",
+	  "worksFor": {
+	    "@type": "Organization",
+	    "name": "Ruckus Labs"
+	  }  
+	}
+	</script>
+
 </body>
 </html>
