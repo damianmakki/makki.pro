10232199ad205c6c548b5c1128b1be6470590377
Damian Makki <damian@makki.pro>
2019-01-30 11:40:24 -0800

    Update makki.pro landing page for 2019.

--- public/index.php
+++ public/index.php
@@ -1,23 +1,49 @@
 <?php $pageTitle='Damian Makki'; ?>
-<?php $pageDescription='Damian Makki is a creative web and print designer based out of New York City.'; ?>
+<?php $pageDescription='Damian Makki is a traveler, designer, and developer currently based out of San Diego.'; ?>
 
 <?php include('components/header.php'); ?>
 
-<div class="content home">
-	<div class="container">
-		<h1>Damian Makki</h1>
-		<h2>A traveler, designer, and developer.<br>Co-founder of <a href="https://sammi.ch">Sammich Shop.</a></h2>
-	</div>
+<div class="container">
 
-	<div class="container">
-		<ul class="social">
-			<li><a href="https://instagram.com/damianmakki/"><img src="assets/images/instagram.svg" alt="Instagram"</a></li>
-			<li><a href="https://dribbble.com/damianmakki"><img src="assets/images/Dribbble.svg" alt="Dribbble"</a></li>
-			<li><a href="https://github.com/damianmakki"><img src="assets/images/github.svg" alt="Github"</a></li>
-			<li><a href="https://www.linkedin.com/in/damianmakki"><img src="assets/images/linkedin.svg" alt="LinkedIn"</a></li>
-			<li><a href="https://www.strava.com/athletes/6190541"><img src="assets/images/strava.svg" alt="Strava"</a></li>
-			<li><a href="https://www.facebook.com/damianmakki"><img src="assets/images/facebook.svg" alt="Facebook"</a></li>
-			<li><a href="https://twitter.com/damianmakki"><img src="assets/images/twitter.svg" alt="Twitter"</a></li>
+	<img src="assets/images/damian-makki-profile.jpg" alt="A photo of Damian Makki in Burano, Italy." />
+
+	<div class="content">
+		<h1>Damian Makki</h1>
+		<h2>Since 2011, I've worked as a freelancer, designer, and front-end developer helping clients and businesses build beautiful, functional products.</h2>
+		<h2>I currently work at <a href="https://www.ayahealthcare.com">Aya Healthcare</a> and run a design studio called <a href="https://ruckuslabs.co">Ruckus Labs</a>.</h2>
+		<h3>Projects</h3>
+		<ul>
+			<li><a href="https://ruckuslabs.co">Ruckus Labs</a></li>
+			<li><a href="/verycool">Very Cool Weekly</a></li>
+			<li><strike>Sammich Shop</strike></li>
+		</ul>
+		<h3>Companies</h3>
+		<ul>
+			<li><a href="https://www.ayahealthcare.com">Aya Healthcare</a></li>
+			<li><a href="https://www.vuforia.com/">Vuforia</a></li>
+			<li><a href="https://www.lawline.com/">Lawline</a></li>
+			<li><a href="https://gocurb.com/">CURB</a></li>
+			<li><a href="https://www.stevens.edu/">Steven's Institute of Technology</a></li>
+			<li><a href="https://www.vividsoftwaresolutions.com/">Vivid Software Solutions</a></li>
+		</ul>
+		<h3>Writing & Speaking</h3>
+		<ul>
+			<li><a href="https://medium.com/@damianmakki">Medium</a></li>
+			<li><a href="http://www.narwhals.cool/33">Narwhals Podcast</a></li>
+		</ul>
+		<h3>Social</h3>
+		<ul>
+			<li><a href="https://instagram.com/damianmakki/">Instagram</a></li>
+			<li><a href="https://dribbble.com/damianmakki">Dribbble</a></li>
+			<li><a href="https://github.com/damianmakki">GitHub</a></li>
+			<li><a href="https://www.linkedin.com/in/damianmakki">LinkedIn</a></li>
+			<li><a href="https://www.strava.com/athletes/6190541">Strava</a></li>
+			<li><a href="https://www.facebook.com/damianmakki">Facebook</a></li>
+			<li><a href="https://twitter.com/damianmakki">Twitter</a></li>
+		</ul>
+		<h3>Contact</h3>
+		<ul>
+			<li><a href="mailto:&#100;&#097;&#109;&#105;&#097;&#110;&#064;&#109;&#097;&#107;&#107;&#105;&#046;&#112;&#114;&#111;">damian@makki.pro</a></li>
 		</ul>
 	</div>
 
